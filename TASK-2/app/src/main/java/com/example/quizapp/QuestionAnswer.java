5ec5dd30336a05daedf7bff3eb7b173c40f93dae
Somanikaushik43 <somanikaushik43@gmail.com>
2023-07-30 14:36:48 +0530

    uploaded files

---
+++ TASK-2/app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,24 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+
+    public static String question[]={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Where you are watching this video?"
+    };
+    public static String choices[][]={
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","Youtube"}
+    };
+    public static String correctAnswers[]={
+            "Google",
+            "Notepad",
+            "Youtube"
+    };
+}
+
+
+
+
